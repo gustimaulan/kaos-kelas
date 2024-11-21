d839bbbf0da1aab2aa558991fbb0ece598dd267c
G Dhia <gmaulan47@gmail.com>
2024-11-21 08:15:17 +0700

    fixing hero

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,19 @@
+<template>
+    <section class=" text-gray-900 p-12 text-center min-h-[400px] flex flex-col justify-center border-b ">
+      <h1 class="text-4xl font-bold">Hiasi Kenangan Sekolahmu dengan Kaos Kelas yang Keren!</h1>
+      <p class="mt-4 text-lg">Buat kaos khusus untuk kelasmu! Sablon berkualitas, desain yang bisa kamu custom sendiri, dan pastinya momen tak terlupakan.</p>
+      <div class="flex flex-col justify-center items-end h-96 mt-4 bg-gray-200 relative"> 
+        <img src="/images/sablon-kaos-satuan-purwokerto.jpeg" alt="Kaos Kelas" class="w-full h-full object-cover object-center rounded-md" />
+      </div>
+      <p class="text-sm italic text-gray-5 absolute left-1/2 transform -translate-x-1/2 text-white/50">property of: atlasprinting_</p>
+      <div class="w-full flex p-2 bg-slate-900 items-center mx-auto justify-center fixed bottom-0 left-0">
+        <a href="#order" class=" w-full py-3 text-lg font-semibold px-8 rounded bg-yellow-300 text-black">Buat Kaos Kelasmu Klik Disini!</a>
+      </div>
+    </section>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HeroSection',
+  }
+  </script>
