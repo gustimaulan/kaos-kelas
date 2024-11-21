0293af7c3ce48341dac8d2829f183be4e5d24882
G Dhia <gmaulan47@gmail.com>
2024-11-21 08:37:26 +0700

    fixing wa

---
+++ src/components/WhatsAppButton.vue
@@ -0,0 +1,49 @@
+<template>
+    <button
+      @click="handleWhatsAppClick"
+      class="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+    >
+      Chat ke WhatsApp
+    </button>
+  </template>
+  
+  <script>
+  export default {
+    name: 'WhatsAppButton',
+    props: {
+      message: {
+        type: String,
+        default: 'Halo Kak Admin atlasprinting_, Saya mau tanya seputar kaos sablon..',
+      },
+      phoneNumber: {
+        type: String,
+        default: '6285161323400',
+        required: true,
+      },
+    },
+    methods: {
+      handleWhatsAppClick() {
+        // Define the message to send
+        const encodedMessage = encodeURIComponent(this.message);
+        const whatsappLink = `whatsapp://send?phone=${this.phoneNumber}&text=${encodedMessage}`;
+  
+        // Open the WhatsApp link
+        window.open(whatsappLink, '_blank');
+  
+        // Track the Add to Cart event
+        fbq('track', 'AddToCart', {
+          content_name: 'Kaos Sablon',
+          content_ids: ['Kaos Sablon'],
+          content_type: 'product',
+          content_category: 'Kaos Sablon',
+          value: 0,
+          currency: 'IDR',
+        });
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Optional: Add custom styles if needed */
+  </style>
