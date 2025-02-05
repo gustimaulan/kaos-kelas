b7e25d0c7b8b011b09f755bd31af92a2ef87e6a8
G Dhia <gmaulan47@gmail.com>
2025-02-05 12:10:57 +0700

    edit pricing table

--- src/components/PriceTable.vue
+++ src/components/PriceTable.vue
@@ -3,14 +3,9 @@
       <h2 class="text-3xl font-bold text-center mb-8">List Harga Sablon Kaos Kelas</h2>
       <div class="grid grid-cols-1 gap-4">
         <div class="mb-4 bg-gray-100 rounded-b-sm">
-          <img src="/images/harga-sablon-dtf-purwokerto.webp" alt="Kaos Kelas Contoh 1"
+          <img src="/images/harga-sablon-kaos-dtf-purwokerto.webp" alt="Kaos Kelas Contoh 1"
             class="rounded-t shadow">
             <p class="text-center py-2">Harga Satuan dan Grosir</p>
-        </div>
-        <div class="mb-4 bg-gray-100 rounded-b-sm">
-          <img src="/images/harga-sablon-kaos-dtf-purwokerto.webp" alt="Kaos Kelas Contoh 2"
-            class="rounded-t shadow">
-            <p class="text-center py-2">Harga Reseller (minimal 12 pcs)</p>
         </div>
     </div>
     </div>
