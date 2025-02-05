0c570f1fb1231c55da3c615a24a4fc2e86b88059
G Dhia <gmaulan47@gmail.com>
2025-02-05 12:29:27 +0700

    edit purge to content on tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-    darkMode: media, // or 'media' or 'class'
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
